3969c77203c6a110069851528b3fe606879f9d08
Alonso Herreros <98601593+alonso-herreros@users.noreply.github.com>
2025-04-26 07:43:29 +0200

    Implementar respuestas ICMP en el router (#17)

--- src/simple_router.py
+++ src/simple_router.py
@@ -18,10 +18,13 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ether
+from ryu.ofproto import inet
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types as etypes
 from ryu.lib.packet import ipv4
+from ryu.lib.packet import icmp as packet_icmp
 from ipaddress import ip_network, ip_address
 
 # TODO: this is a hard copy of info in scenario.py. Gotta change that.
@@ -49,7 +52,7 @@ class SimpleRouter(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(SimpleRouter, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        self._mac_port_table = {}
         # This routing doesn't support next hop, just output interface.
         # I'm making a distinction between 'interfaces' data from INTERFACES
         # and 'ports' data from the datapath
@@ -57,6 +60,10 @@ class SimpleRouter(app_manager.RyuApp):
             {'net': ip_network(iface['ip'], strict=False),
              'out_iface': n}
             for n, iface in enumerate(INTERFACES['s1'])
+        ]
+        self.ip_addresses = [
+            iface['ip'].split('/')[0]
+            for iface in INTERFACES['s1']
         ]
         self.arpDict = {
             iface['ip'].split('/')[0]: iface['mac']
@@ -123,13 +130,13 @@ class SimpleRouter(app_manager.RyuApp):
 
         # ---- Learn from packet ----
         # Learn a mac address
-        self.mac_to_port.setdefault(dpid, {})
-        self.mac_to_port[dpid][src] = in_port
+        self._mac_port_table.setdefault(dpid, {})
+        self._mac_port_table[dpid][src] = in_port
 
         # ---- Process packet ----
         if eth.dst == datapath.ports[in_port].hw_addr:
             # Router is the destination - Hand over to specific handler
-            self._packet_rcv_handler(msg, pkt)
+            self._packet_rcv_handler(msg, pkt, eth)
         else:
             # Forward the ethernet packet as a switch
             self._eth_fw_handler(msg, pkt, eth)
@@ -142,11 +149,7 @@ class SimpleRouter(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            self.logger.info(' no match, flooding')
-            out_port = ofproto.OFPP_FLOOD
+        out_port = self.mac_to_port(datapath, eth.dst)
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
@@ -164,24 +167,104 @@ class SimpleRouter(app_manager.RyuApp):
         self.msg_out(msg, actions)
 
 
-    def _packet_rcv_handler(self, msg, pkt):
+    def _packet_rcv_handler(self, msg, pkt, eth):
         self.logger.info(' Handling rcv')
         ip = pkt.get_protocol(ipv4.ipv4)
-        if ip:  self._ip_in_handler(msg, pkt, ip)
+        if ip:  self._ip_in_handler(msg, pkt, eth, ip)
         else:   self.logger.info('  Not IP, we can\'t handle this')
 
 
-    def _ip_in_handler(self, msg, pkt, ip):
+    def _ip_in_handler(self, msg, pkt, eth, ip):
         self.logger.info(f' Handling: IPv4 {ip.src} to {ip.dst} ({ip.ttl})')
 
-        actions = self.ip_fw_actions(msg.datapath, ip)
+        if ip.dst in self.ip_addresses:
+            self._ip_rcv_handler(msg, pkt, eth, ip)
+        else:
+            self._ip_fw_handler(msg, pkt, eth, ip)
+
+
+    def _ip_fw_handler(self, msg, pkt, eth, ip):
+        datapath = msg.datapath
+        ip_dest = ip_address(ip.dst)
+
+        out_port = self.ip_to_port(datapath, ip_dest)
+
+        if not out_port:  return None
+
+        self.logger.info(f'  route match to output port {out_port}')
+
+        actions = self.actionsForward(
+            datapath, out_port,
+            datapath.ports[out_port].hw_addr, self.arpDict[ip.dst]
+        )
 
         self.msg_out(msg, actions)
 
 
-    def ip_fw_actions(self, datapath, ip):
-        ip_dest = ip_address(ip.dst)
+    def _ip_rcv_handler(self, msg, pkt, eth, ip):
+        self.logger.info(' Handling IP rcv')
+        header = pkt.get_protocol(packet_icmp.icmp)
+        if header:  self._icmp_rcv_handler(msg, pkt, eth, ip, header)
+        else:     self.logger.info('  Got an IP packet, now what?')
 
+
+    def _icmp_rcv_handler(self, msg, pkt, eth, ip, icmp):
+        self.logger.info(' Handling ICMP rcv')
+        if icmp.type == packet_icmp.ICMP_ECHO_REQUEST:
+            # Send echo response back
+            self.send_icmp_echo_reply(msg, pkt, eth, ip, icmp)
+
+
+    def send_icmp_echo_reply(self, msg, pkt, eth, ip, icmp):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        out_eth = ethernet.ethernet(dst=eth.src, src=eth.dst,
+                ethertype=ether.ETH_TYPE_IP)
+
+        # Type 0 Code 0 is Echo Reply
+        out_icmp = packet_icmp.icmp(0, code=0, csum=0, data=icmp.data)
+
+        ip_total_length = ip.header_length * 4 + out_icmp._MIN_LEN
+        if out_icmp.data:
+            ip_total_length += out_icmp.data._MIN_LEN
+            if out_icmp.data.data:
+                ip_total_length += len(out_icmp.data.data)
+
+        out_ip = ipv4.ipv4(ip.version, ip.header_length, ip.tos,
+                           ip_total_length, ip.identification, ip.flags,
+                           ip.offset, ttl=64, proto=inet.IPPROTO_ICMP,
+                           src=ip.dst, dst=ip.src)
+
+        pkt = packet.Packet()
+        pkt.add_protocol(out_eth)
+        pkt.add_protocol(out_ip)
+        pkt.add_protocol(out_icmp)
+        pkt.serialize()
+
+        # Send packet out
+        self.logger.info(' Sending ICMP echo response')
+        actions = [parser.OFPActionOutput(ofproto.OFPP_IN_PORT, 0)]
+        datapath.send_packet_out(buffer_id=0xffffffff, in_port=in_port,
+                                 actions=actions, data=pkt.data)
+
+
+
+    def msg_out(self, msg, actions):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+
+    def ip_to_port(self, datapath, ip_dest):
         out_port = None
         for rule in self.routes:
             if ip_dest in rule['net']:
@@ -194,24 +277,13 @@ class SimpleRouter(app_manager.RyuApp):
                     ))
                 out_port = rule['out_port']
 
-        if not out_port:  return None
+        return out_port
 
-        self.logger.info(f'  route match to output port {out_port}')
-        return self.actionsForward(
-            datapath, out_port,
-            datapath.ports[out_port].hw_addr, self.arpDict[ip.dst]
-        )
 
-    def msg_out(self, msg, actions):
-        datapath = msg.datapath
+    def mac_to_port(self, datapath, mac_dst):
+        dpid = datapath.id
         ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
-
-        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        return self._mac_port_table[dpid].get(mac_dst, ofproto.OFPP_FLOOD)
 
 
     def actionsForward(self, datapath, port, src, dst):
